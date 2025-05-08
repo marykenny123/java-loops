49d49060cb46c5a2cccf3cbff858c64c18a47456
mary kenny <marykenny2704@yahoo.com>
2025-05-08 14:10:06 +0200

    docs/commit of ForLoops

--- LogIn.java
+++ LogIn.java
@@ -1,6 +1,8 @@
 public class LogIn {
     public static void main(String[] args) {
-        //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, escribe un bucle que siga ejecutándose y devuelva 'Datos incorrectos por favor vuelve a intentar' mientras el nombre de usuario o la contraseña sean incorrectos.
+        //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, escribe un bucle
+        // que siga ejecutándose y devuelva 'Datos incorrectos por favor vuelve a intentar' mientras el nombre de
+        // usuario o la contraseña sean incorrectos.
         //Cuando el usuario y la constraseña sean correctos imprime en terminal: ¡Acceso concedido!
 
         //String correctUsername = "Pepita";
--- WhileLoops.java
+++ WhileLoops.java
@@ -1,17 +1,54 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
+/*
+        // Mary -> he añadido de imprimir el num aleatorio y num de bucles para revisar, dado que el ej no lo pide lo
+        // he dejado comentado
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7. Has de usar la función getRandomNumber
+        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7. Has de usar la función
+        // getRandomNumber
+        int random = 1;
+        int i =1; //   para mi control
+        while (!(random == 7)) {
+            random = (getRandomNumber(50));
+            //System.out.println("El número aleatorio es " + random + " y ha corrido el bucle " + i + " veces.");
+            //i++;
+        }
 
+        System.out.println("\n");
 
+        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70. Has de usar la
+        // función getRandomNumber
+        while (!(random >= 70)) {
+            random = (getRandomNumber(150));
+                //System.out.println("El núm aleatorio es " + random + " y ha corrido el bucle " + i + " veces.");
+                //i++;
+        }
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70. Has de usar la función getRandomNumber
-        
+        System.out.println("\n");
+*/
+        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100. El usuario debe adivinar el número
+        // aleatorio y el programa debe seguir solicitando intentos hasta que el usuario adivine correctamente. Después
+        // de cada intento, el programa debe indicar si el número ingresado es mayor o menor que el número generado.
+        // Has de usar la función getRandomNumber
 
-
-        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100. El usuario debe adivinar el número aleatorio y el programa debe seguir solicitando intentos hasta que el usuario adivine correctamente. Después de cada intento, el programa debe indicar si el número ingresado es mayor o menor que el número generado. Has de usar la función getRandomNumber
-
-    
+        int random = 1;
+        int userGuess;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("P fv teclea tu número entre 1 y 100 y dar a intro.\t");
+        random = getRandomNumber(100);
+        do {
+            userGuess = scanner.nextInt();
+            if (random > userGuess) {
+                System.out.println("Tu número es menor que el número aleatorio.");
+            } else if (random < userGuess){
+                System.out.println("Tu número es mayor que el número aleatorio.");
+            }
+        }
+            while (!(userGuess == random));
+        System.out.println("Has acertado.");
+        scanner.close();
     }
 
     /**
